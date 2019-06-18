037696bfe1260f7d1b473311c80eebf37c78e926
Arnold <397136899@qq.com>
2019-06-18 11:44:27 +0800

    use openal for andorid

---
+++ android-specific/openal/prebuilt/android/Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := ext_openal
+LOCAL_MODULE_FILENAME := openal
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/openal.a
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include
+include $(PREBUILT_STATIC_LIBRARY)
